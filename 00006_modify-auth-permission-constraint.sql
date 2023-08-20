3d8014d99234444560b29d1c410ad0683ed0628b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-20 12:30:04 -0500

    chore: modify auth_permission constraint in migration file
    The auth_permission constraint in the migration file is modified to add
    a unique constraint on the combination of permission and resource
    columns. This change ensures that each permission-resource combination
    is unique in the auth_permission table.

--- 00006_modify-auth-permission-constraint.sql
+++ 00006_modify-auth-permission-constraint.sql
@@ -4,8 +4,3 @@ ALTER TABLE auth_permission
     DROP constraint auth_permission_c1,
     ADD CONSTRAINT auth_permission_c1 UNIQUE(permission,resource);
 
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE auth_permission
-    DROP constraint auth_permission_c1,
-    ADD CONSTRAINT auth_permission_c1 UNIQUE(permission);
