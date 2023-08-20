95473422d5b9c33aa846d3baa8a0c01edebfd84d
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-20 13:15:44 -0500

    fix(install_schema.sh): change order of arguments in psql command to fix incorrect argument placement
    The order of arguments in the psql command has been corrected to ensure
    that the CREATE EXTENSION statement is executed correctly. The -c flag
    is now placed before the database name argument ($1) to ensure that the
    extension is created in the correct database.

--- install_schema.sh
+++ install_schema.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PGPASSWORD=${PASS}
-psql -U ${USER} -h ${HOST} --port ${PORT} -f ${sql} -d $1 -c "CREATE EXTENSION IF NOT EXISTS citext;"
+psql -U ${USER} -h ${HOST} --port ${PORT} -f ${sql} -c "CREATE EXTENSION IF NOT EXISTS citext;" $1
 for sql in /usr/src/appdata/*.sql; do 
 	psql -U ${USER} -h ${HOST} --port ${PORT} -f ${sql} -d $1
 done
