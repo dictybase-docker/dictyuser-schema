087991470dd88661032b6304fbf6138124e38bfd
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-20 17:38:47 -0500

    refactor(install_schema.sh): remove unnecessary environment variables and simplify psql commands
    The installation script no longer requires the PGPASSWORD, USER, HOST,
    and PORT environment variables as they are not used in the psql
    commands.

--- install_schema.sh
+++ install_schema.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-export PGPASSWORD=${PASS}
-psql -U ${USER} -h ${HOST} --port ${PORT} -f ${sql} -c "CREATE EXTENSION IF NOT EXISTS citext;" $1
+psql -c "CREATE EXTENSION IF NOT EXISTS citext;"
 for sql in /usr/src/appdata/*.sql; do 
-	psql -U ${USER} -h ${HOST} --port ${PORT} -f ${sql} -d $1
+	psql -f ${sql}
 done
