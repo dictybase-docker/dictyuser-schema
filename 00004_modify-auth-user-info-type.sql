e45c40e919ac3613622e1b99afdfc4a56c3914f4
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-20 12:29:39 -0500

    chore: modify auth_user_info type in migration file
    The migration file "00004_modify-auth-user-info-type.sql" has been
    modified to change the data types of columns in the "auth_user_info"
    table. The "city", "state", "zipcode", "country", and "phone" columns
    have been changed from varchar to text data type. This modification
    improves the flexibility and storage capacity of the columns.

--- 00004_modify-auth-user-info-type.sql
+++ 00004_modify-auth-user-info-type.sql
@@ -7,11 +7,3 @@ ALTER TABLE auth_user_info
     ALTER COLUMN country TYPE text,
     ALTER COLUMN phone TYPE text;
 
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE auth_user_info
-    ALTER COLUMN city TYPE varchar(255),
-    ALTER COLUMN state TYPE varchar(50),
-    ALTER COLUMN zipcode TYPE varchar(30),
-    ALTER COLUMN country TYPE varchar(20),
-    ALTER COLUMN phone TYPE varchar(30);
