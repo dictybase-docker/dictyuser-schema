325aea95261d89fca132eb3e4d1de4b8840a7e94
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-20 13:06:51 -0500

    feat(install_schema.sh): add creation of citext extension to the database schema installation script
    The script now includes a step to create the citext extension in the
    database schema installation process. This extension allows for
    case-insensitive text comparison and searching in the database.

--- install_schema.sh
+++ install_schema.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PGPASSWORD=${PASS}
+psql -U ${USER} -h ${HOST} --port ${PORT} -f ${sql} -d $1 -c "CREATE EXTENSION IF NOT EXISTS citext;"
 for sql in /usr/src/appdata/*.sql; do 
 	psql -U ${USER} -h ${HOST} --port ${PORT} -f ${sql} -d $1
 done
