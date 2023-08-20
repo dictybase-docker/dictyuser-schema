2ec4eb136a7ed9db43bc5245c11824b3bc3b89d2
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-20 12:29:52 -0500

    feat(database): add auth_permission resource column
    The auth_permission table now has a new column called "resource" of type
    text. This column is used to store the resource associated with a
    permission. This change allows for more granular control and management
    of permissions within the application.

--- 00005_add-auth-permission-column.sql
+++ 00005_add-auth-permission-column.sql
@@ -3,7 +3,3 @@
 ALTER TABLE auth_permission
     ADD COLUMN resource text;
 
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE auth_permission
-    DROP COLUMN resource;
